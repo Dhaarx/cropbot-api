a9f1f7af6ea8e732c31bdae89a2523c6c2a54ee5
DHARINEESH N M <136150401+Dhaarx@users.noreply.github.com>
2026-04-12 22:03:21 +0530

    Update model version in livestock_query_gemini function

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ def crop_query_gemini(user_input):
 
 def livestock_query_gemini(user_input):
     response = client.models.generate_content(
-        model='gemini-2.0-flash',
+        model='gemini-2.5-flash',
         contents=user_input,
         config=types.GenerateContentConfig(
             system_instruction=livestock_system_prompt,
